394d6a84c7adb81ee7275d1f28c5bde6ee0cde24
AntonKryshniou <akryshnev@mail.ru>
2021-09-08 16:25:37 +0300

    new

---
+++ main.py
@@ -0,0 +1,54 @@
+
+a = "my"
+b = 1
+c = 1.5
+# print(a, b, c)
+#
+d = b'0-123'
+#
+# print(a, b, c, d)
+#
+# print (type(d), d, type(a), a, type(b), b, type(c), c )
+
+e = ['eli', 'myaso', 'muzhiki']
+# print(type(e), e)
+
+f = ("mein", "herz", "brennt")
+# print (f, type(f))
+
+g = {"eins", "zwei", "drei", "uno", "dos", "uno", "drei"}
+# print (g, type(g))
+
+h = frozenset("elka")
+
+# print(h, type(h))
+
+i = {}
+
+# print(i, type(i))
+
+print(a, type(a), b, type(b),c, type(c),d, type(d), e, type(e),f, type(f), g, type(g), h, type(h),i, type(i))
+
+
+j = "uno"
+k = "dos"
+l = k+j
+
+print(l)
+
+m = 123
+n = 2
+o = m+n
+print(o)
+
+p = m%n
+print (p)
+
+q = n*m
+print(q)
+
+r = m//n
+print(r)
+
+s = m/n
+print(s)
